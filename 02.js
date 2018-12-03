b8be3ee8ebc9dcea972c4098469740167769b365
Tiago Garcia <tiagoromerogarcia@gmail.com>
2018-12-02 19:13:03 -0800

    better structuring

--- 02.js
+++ 02.js
@@ -33,14 +33,15 @@ function readLines(file, onLine) {
     console.log(`The checksum is ${checksum}`);
 })();
 
+// Compares two strings to see if they differ by one char and which one 
 function compare(string1, string2) {
     const length = string1.length;
     let differentChars = 0;
     let differIndex;
     for (let i = 0; i < length; i++) {
         if (string1.charAt(i) !== string2.charAt(i)) {
-            differIndex = i;
-            differentChars++;
+            differentChars++;            
+            differIndex = differentChars === 1 ? i : undefined;
         }
     }
 
@@ -50,24 +51,30 @@ function compare(string1, string2) {
     };
 }
 
-(async function part2() {    
+// Compare each strings to every other string 
+// and get the common letters in case the differByOneChar is true
+function getCommonLetters(ids) {
+    const idsCount = ids.length;
+    for (let i = 0; i < idsCount; i++) {
+        const id = ids[i];
+        for (let j = i + 1; j < idsCount; j++) {
+            const { differByOneChar, differIndex } = compare(id, ids[j]);
+            if (differByOneChar) {
+                return id.slice(0, differIndex) + id.slice(differIndex + 1);
+            }
+        }
+    }
+}
+
+(async function part2() {
+    // Reading each line an adding into the ids array
     const ids = [];
     const onLine = line => {
         ids.push(line);
     }
-    await readLines('02-input.txt', onLine);
-        
-    const idsCount = ids.length;
-    for (let i = 0; i < idsCount; i++) {
-        const id = ids[i];
-        for (let j = i+1; j < idsCount; j++) {
-            const { differByOneChar, differIndex } = compare(id, ids[j]);
-            if (differByOneChar) {
-                const commonLetters = id.slice(0, differIndex) + id.slice(differIndex + 1);
-                console.log(`The common letters are ${commonLetters}`);
-                return;
-            }
-        }
-    }
+    await readLines('02-input.txt', onLine);    
+
+    const commonLetters = getCommonLetters(ids);
+    console.log(`The common letters are ${commonLetters}`);
 
 })();
