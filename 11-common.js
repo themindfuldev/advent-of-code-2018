914feee2347fe294de6391ad0cb108e7f0d545b6
Tiago Romero Garcia <tiago@Tiago-Air.hsd1.ca.comcast.net>
2018-12-11 12:01:29 -0800

    optimizing!

--- 11-common.js
+++ 11-common.js
@@ -17,17 +17,34 @@ const buildGrid = serialNumber => {
     return grid;
 };
 
-const findPowerSquare = (grid, squareSize) => {
+const findPowerSquare = (grid, squareSize, cache = new Map()) => {
     let squarePower;
     let top = 0;
     let left = 0;
+
     for (let i = 1; i <= 300 - squareSize; i++) {
         for (let j = 1; j <= 300 - squareSize; j++) {
             let power = 0;
-            for (let k = i; k < i + squareSize; k++) {
-                for (let l = j; l < j + squareSize; l++) {
-                    power += grid[k][l];
+
+            const previousKey = `${i},${j},${squareSize-1}`;
+            const currentKey = `${i},${j},${squareSize}`;
+            if (cache.has(previousKey)) {
+                power = cache.get(previousKey);
+                for (let k = i; k < i + squareSize - 1; k++) {
+                    power += grid[k][j + squareSize - 1];
                 }
+                for (let k = j; k < j + squareSize; k++) {
+                    power += grid[i + squareSize - 1][k];
+                }
+                cache.set(currentKey, power);
+            }
+            else {
+                for (let k = i; k < i + squareSize; k++) {
+                    for (let l = j; l < j + squareSize; l++) {
+                        power += grid[k][l];
+                    }
+                }
+                cache.set(currentKey, power);
             }
             if (squarePower === undefined || power > squarePower) {
                 top = i;
--- 11b.js
+++ 11b.js
@@ -9,8 +9,9 @@ const findPowerSquareOfAnySize = grid => {
     let left = 0;
     let size = 0;
 
+    const cache = new Map();
     for (let i = 1; i <= 300; i++) {
-        const [x, y, power] = findPowerSquare(grid, i);
+        const [x, y, power] = findPowerSquare(grid, i, cache);
 
         if (squarePower === undefined || power > squarePower) {
             top = x;
@@ -26,6 +27,7 @@ const findPowerSquareOfAnySize = grid => {
 (() => {
     const serialNumber = 5719;
     const grid = buildGrid(serialNumber);    
+
     const [top, left, size, squarePower] = findPowerSquareOfAnySize(grid);
     
     console.log(`The X,Y,size is ${top},${left},${size} and total power is ${squarePower}`);
